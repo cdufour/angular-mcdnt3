0b9181aa39462f53abc43a46e97e78edb36f6655
cdufour <opusidea@gmail.com>
2018-04-11 17:02:30 +0200

    J3 pm end

--- src/app/cup/cup.component.ts
+++ src/app/cup/cup.component.ts
@@ -6,11 +6,16 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./cup.component.css']
 })
 export class CupComponent implements OnInit {
-  @Input('nb') nb: number = 8;
+  @Input('nb') nb: number = 4;
+  markup: string = '';
 
   constructor() { }
 
   ngOnInit() {
+    let img_markup = '<img class="cup_small" src="/assets/img/cup.png" alt="">';
+    for(let i=0; i<this.nb; i++) {
+      this.markup += img_markup;
+    }
   }
 
 }
--- src/app/teams/teams.component.ts
+++ src/app/teams/teams.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Team } from '../../model/Team';
+import { COUNTRIES } from '../datasources';
 
 @Component({
   selector: 'teams',
@@ -8,17 +9,58 @@ import { Team } from '../../model/Team';
 })
 export class TeamsComponent implements OnInit {
   teams: Team[] = [];
+  teamsFiltered: Team[] = []; // tableau servant à stocker les retours de la function .filter
+
   team_display: string;
+  countries: string[] = [];
+  test: boolean = true;
+  countrySelected: string;
 
   constructor() { }
 
   ngOnInit() {
+    this.countries = COUNTRIES;
     this.team_display = 'a2';
-
     let team1: Team = new Team('Juventus', 'juve-logo.png', 'Allegri', 1897, 'Juventus Stadium', 5, 'Italie');
     let team2: Team = new Team('PSG', 'psg-logo.png', 'Emery', 1970, 'Parc des Princes', 3, 'France');
     let team3: Team = new Team('Strasbourg', 'strasbourg-logo.png', 'Laurey', 1906, 'La Meinau', 1, 'France');
-    this.teams = [team1, team2, team3];
+    this.teams = [team1, team2, team3]; // initialisation de teams
+    this.teamsFiltered = this.teams; // initialisation de teamsFiltered
   }
 
+  addTeam() {
+    let steaua: Team =
+      new Team('Steaua', 'psg-logo.png', 'Vlad Tepes', 1602, 'Ghencea', 6, 'Roumanie');
+
+    // ajout de l'objet steaua au tableau this.teams
+    this.teams.push(steaua);
+    //this.teams.splice(0, 1);
+  }
+
+  filterTeam() {
+    //console.log(this.countrySelected);
+
+    if (this.countrySelected == '0') {
+      // l'utilisateur n'a pas choisi de pays => afficher toutes les équipes
+      this.teamsFiltered = this.teams;
+    } else {
+      this.teamsFiltered =
+        this.teams.filter((team: Team) => team.country == this.countrySelected);
+    }
+
+  }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* Tanguy Style */
